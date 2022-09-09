214cd5d5c62a05bd6132ff018f57e69088393b36
Aidan Praytor <aidanpraytor@berkeley.edu>
2022-09-08 18:02:07 -0700

    docstrings added

--- manager.py
+++ manager.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""Control portion of project. Handles the automated calling of scraper.py and the writing of
+data to data/allPulls.csv.
+"""
+
 import csv
 import datetime
 import scraper
@@ -7,17 +12,36 @@ weekdayHours = (7, 23)
 saturdayHours = (8, 18)
 sundayHours = (8, 23)
 
-pullsPerHour = 6
+pullsPerHour = 12
 
 
 def pull_and_write(now):
+    """Call scraper.retrieve(), write returned data to .csv
+
+    :param now: datetime object containing current year, month, day, hour, minute, second
+    """
     with open('data/allPulls.csv', mode='a') as data:
         data_writer = csv.writer(data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         data_writer.writerow([now.strftime("%Y"), now.strftime("%d"),
-                              now.strftime("%a"), now.strftime("%H:%M"), scraper.retrieve()])
+                              now.strftime("%a"), now.strftime("%H:%M"),
+                              scraper.retrieve(url='https://safe.density.io/#/displays/dsp_956223069054042646?token'
+                                                   '=shr_o69HxjQ0BYrY2FPD9HxdirhJYcFDCeRolEd744Uj88e')])
 
 
-def check(cdt):
+def check(cdt, PPH):
+    """Controls automated scraping and logging of data.
+
+    Given datetime object, cdt, determines current day-of-week and consequently the RSF hours of
+    operation. Determines if the current time is before, after, or during RSF hours. If before,
+    determines time until RSF opens, sleeps until then. If after, determines time until next day,
+    sleeps until then. If during, calls pull_and_write with current datetime object, then sleeps
+    for regular interval determined by PPH (pulls per hour). Function recursively calls itself
+    again every time sleep command expires.
+
+    :param PPH: Number of times the program should gather and log data per hour, given that
+    it is started on the hour.
+    :param cdt: datetime object containing current year, month, day, hour, minute, second
+    """
     currentHour = int(cdt.strftime("%H"))
     currentMinute = int(cdt.strftime("%M"))
     currentTime = currentHour + (currentMinute / 60)
@@ -35,14 +59,14 @@ def check(cdt):
     elif currentTime > closesAt:
         timeUntilNextDay = ((24 - currentTime) * 3600) + 60  # just for good measure
         print("The RSF has already closed. I'll sleep for " + str(timeUntilNextDay)
-                                                            + " seconds until tomorrow and check again!")
+              + " seconds until tomorrow and check again!")
         time.sleep(timeUntilNextDay)
     else:
         pull_and_write(cdt)
-        minutesUntilNextPull = (60 // pullsPerHour)
+        minutesUntilNextPull = (60 // PPH)
         print("Data pulled. I'll sleep for " + str(minutesUntilNextPull) + " minute(s), and then pull again!")
         time.sleep(minutesUntilNextPull * 60)
-    check(datetime.datetime.now())
+    check(datetime.datetime.now(), PPH)
 
 
-check(datetime.datetime.now())
+check(datetime.datetime.now(), pullsPerHour)
--- scraper.py
+++ scraper.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+"""Scraping portion of project. Conducts scrape of RSF Crowd Meter website and returns target data."""
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -6,8 +9,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.ui import WebDriverWait
 
-url = 'https://safe.density.io/#/displays/dsp_956223069054042646?token=shr_o69HxjQ0BYrY2FPD9HxdirhJYcFDCeRolEd744Uj88e'
-
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
@@ -15,7 +16,19 @@ driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager()
 wait = WebDriverWait(driver, 10)
 
 
-def retrieve():
+def retrieve(url):
+    """
+    Scrapes RSF Crowd Meter website, returns target data.
+
+        Using Selenium Chromedriver, opens a headless browser and isolates 'root' element,
+    the parent of target span element, by ID. When 'root' has been found, uses 'root' location
+    in HTML tree to find target span by XPATH. In both of these steps, WebDriverWait is used
+    to allow JS on webpage to load before attempting to locate invisible elements. If either
+    element takes longer than 10 seconds to find, timeout message is printed and System exits 1.
+
+    :param url: url of RSF Crowd Meter tool
+    :return str: .text attribute of targeted span
+    """
     driver.get(url)
     wait.until(presence_of_element_located((By.ID, "root")))
     root = driver.find_element(By.ID, "root")
@@ -24,6 +37,7 @@ def retrieve():
         print("Root found")
     else:
         print("Root not found. Process timed out.")
+        SystemExit(1)
 
     wait.until(presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div/div[1]/div[2]/div/span')))
     target = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[1]/div[2]/div/span')
@@ -32,6 +46,6 @@ def retrieve():
         print("Target found: " + target.text)
     else:
         print("Target not found. Process timed out.")
+        SystemExit(1)
 
-    # driver.quit()
     return target.text
